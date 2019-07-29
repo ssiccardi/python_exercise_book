d1c7abfeaf9f96f98515d501227eda95620b6d28
Stefano <ssiccardi@2ssas.it>
2019-07-29 18:18:58 +0200

    Modified example of NLTK pos tagging

--- test1_nltk.py
+++ test1_nltk.py
@@ -10,15 +10,31 @@ tknzr = TweetTokenizer()
 # export CLASSPATH="/home/openerp/Scrivania/ricerca_lavoro/nltk_prodnames/stanford-postagger-2018-10-16"
 
 def divide_name(s0):
+
+# first: check for typos (this should be made automatically)
+    wrong=[("T Shirt","Tshirt")]
+    for ss in wrong:
+        s0=s0.replace(ss[0],ss[1])
+
+# second get the single words
     s1=tknzr.tokenize(s0)
+
+# third tag the parts of speech: basically nouns will be the end marks of splitted parts
     s2 = nltk.pos_tag(s1)    # standard nltk tagger
 #    s2 = st.tag(s1)     # stanford tagger
     last = 'None'
     out = ""
-    connectors = ["%",","]
+
+    connectors = ["%",","] # this is just to avoid extra spaces before some symbols
+
+# fourth change tags that either are inappropriate (e.g. "big" tagged as a noun) or do not fit our purposes (e.g. "women" is a noun but here likely means "for women" so it is a kind of adjective)
+
     #exceptions = ["%",",","painting","quality","household"]  # test for stanford
-    exceptions = ["%",",","painting","quality","household","canvas","art","nude","beige","sexy","34-46","fashion","round","sports","classic","men","mixed","toe","dog","nest"]
-    exc_tags = [":",":","NN","JJ","JJ","NN","NN","JJ","JJ","JJ","NN","NN","JJ","JJ","JJ","JJ","JJ","NN","JJ","NN"]
+    exceptions = ["%",",","painting","quality","household","canvas","art","nude","beige","sexy","34-46","fashion","round","sports","classic", \
+        "men","mixed","toe","dog","nest","adornment","articles","motorcycle","pants","moto","protective","riding","touring","motorbike","summer","print","women","iron","winter","pointed","low","lady","boots","fur","wool"]
+    exc_tags = [":",":","NN","JJ","JJ","NN","NN","JJ","JJ","JJ","NN","NN","JJ","JJ","JJ","JJ","JJ","NN","JJ","NN","JJ","NN","JJ","NN","JJ","JJ","JJ","JJ","JJ","JJ","JJ","JJ","JJ","JJ","JJ","JJ","JJ","NN","JJ","JJ"]
+
+# fifth compute parts putting together words that aren't nouns + 1 noun, or just 2 nouns
     i=0
     for sx in s2:
         if sx[0].lower() in exceptions:
@@ -28,12 +44,11 @@ def divide_name(s0):
             continue
         if sx[1][:2] == 'NN':
             tmp = nltk.pos_tag(sx[0].lower())
-#            if tmp != 'NNP':
             s2[i] = (sx[0],tmp[0][1])
         i=i+1
-    print(s2)
+#    print(s2) # to check assigned tags
     for sx in s2:
-        if last in ('None', 'JJ', 'JJR', 'JJS', 'CD', 'RB', 'RBR', 'RBS', ':', 'NNP', 'VB', 'VBG','VBN','DT','POS','PDT','IN','CC'):
+        if last in ('None', 'JJ', 'JJR', 'JJS', 'CD', 'RB', 'RBR', 'RBS', ':', 'NNP', 'VB', 'VBG','VBN','DT','POS','PDT','IN','CC','SYM'):
             if not out:
                 cx = 1
                 out = sx[0]
@@ -57,34 +72,33 @@ def divide_name(s0):
         last = sx[1]
     return out
 
-#s0 = "100% hand-painted canvas oil painting high quality Household adornment art flower pictures DM-15072306"
-#print(s0)
-#print(divide_name(s0))
-#s0 = "MORAZORA Big Size 34-46 2019 New Fashion high heels women pumps thin heel classic white red nude beige sexy ladies wedding shoes"
-#print(s0)
-#print(divide_name(s0))
-#s0 = "Luxury Woman Elegant Flats Solid Vintage Genuine Leather Sexy Pointed Toe Classic Low Heels Casual Office Lady Mules P16"
-#print(s0)
-#print(divide_name(s0))
-#s0 = "RELKA Classic Men Fashion Casual Shoes Luxury Flying Round Toe Comfortable Heel Shoes Mixed Color Soft Basic Sports Shoes P95"
-#print(s0)
-#print(divide_name(s0))
-#s0 = "MORAZORA Russia 2019 Genuine leather boots wool fur fashion knee high boots women warm wool boots round toe winter snow boots"
-#print(s0)
-#print(divide_name(s0))
-#s0 = "Pet Dog Bed Warming Dog House Soft Material Nest Dog Baskets Fall and Winter Warm Kennel For Cat Puppy Plus size Drop shipping"
-#print(s0)
-#print(divide_name(s0))
+s0 = "100% hand-painted canvas oil painting high quality Household adornment art flower pictures DM-15072306"
+print(s0)
+print(divide_name(s0))
+s0 = "MORAZORA Big Size 34-46 2019 New Fashion high heels women pumps thin heel classic white red nude beige sexy ladies wedding shoes"
+print(s0)
+print(divide_name(s0))
+s0 = "Luxury Woman Elegant Flats Solid Vintage Genuine Leather Sexy Pointed Toe Classic Low Heels Casual Office Lady Mules P16"
+print(s0)
+print(divide_name(s0))
+s0 = "RELKA Classic Men Fashion Casual Shoes Luxury Flying Round Toe Comfortable Heel Shoes Mixed Color Soft Basic Sports Shoes P95"
+print(s0)
+print(divide_name(s0))
+s0 = "MORAZORA Russia 2019 Genuine leather boots wool fur fashion knee high boots women warm wool boots round toe winter snow boots"
+print(s0)
+print(divide_name(s0))
+s0 = "Pet Dog Bed Warming Dog House Soft Material Nest Dog Baskets Fall and Winter Warm Kennel For Cat Puppy Plus size Drop shipping"
+print(s0)
+print(divide_name(s0))
 s0 = "Ceramic little monk Buddha Statues tea pet creative home furnishing articles small adornment home decor ornament landscape"
 print(s0)
 print(divide_name(s0))
-#s0 = "2018 New Motorcycle Pants Men Moto Jeans Protective Gear Riding Touring Motorbike Trousers Motocross Pants Pantalon Moto Pants"
-#print(s0)
-#print(divide_name(s0))
-#s0 = "Women's Summer Print Jumpsuit Shorts Casual Loose Short Sleeve V-neck Jumpsuit 10 color"
-#print(s0)
-#print(divide_name(s0))
-#s0 = "New 2019 Women Tshirt Batman Spiderman Iron Man Captain America Winter Soldier Marvel T Shirt Avengers Costume Comics Superhero"
-#print(s0)
-#print(divide_name(s0))
-
+s0 = "2018 New Motorcycle Pants Men Moto Jeans Protective Gear Riding Touring Motorbike Trousers Motocross Pants Pantalon Moto Pants"
+print(s0)
+print(divide_name(s0))
+s0 = "Women's Summer Print Jumpsuit Shorts Casual Loose Short Sleeve V-neck Jumpsuit 10 color"
+print(s0)
+print(divide_name(s0))
+s0 = "New 2019 Women Tshirt Batman Spiderman Iron Man Captain America Winter Soldier Marvel T Shirt Avengers Costume Comics Superhero"
+print(s0)
+print(divide_name(s0))
